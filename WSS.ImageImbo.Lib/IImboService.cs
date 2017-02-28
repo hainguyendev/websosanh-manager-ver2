ab0dc3818b46104778692f0438a1b232ab6bed92
xuantrangk54 <xuantrangk54@gmail.com>
2017-02-28 10:23:05 +0700

    Refactory code to test

---
+++ WSS.ImageImbo.Lib/IImboService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSS.ImageImbo.Lib
+{
+    public interface IImboService
+    {
+        string PostImgToImbo(string url, string publicKey, string privateKey, string userName, string host, int port);
+        string PostImgWithChangeTransference(string url, string publicKey, string privateKey, string userName, string host, int port);
+    }
+}
